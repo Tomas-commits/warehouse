67963b6c790181de046956837701c21c10ca1a31
“[firstname <tomaspurvinys@google.com>
2024-01-01 00:31:15 +0200

    This must pass

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('should display total items registered', async ({ page }) => {
-  await page.goto('http://localhost:8080/overview'); // replace with the correct URL to the OverviewView.vue page
+  await page.goto('http://localhost:5173/'); // replace with the correct URL to the OverviewView.vue page
   const totalItemsText = await page.textContent('h5'); // replace 'h5' with the correct selector
   expect(totalItemsText).toContain('registered');
 });
