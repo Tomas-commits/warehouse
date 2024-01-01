85b9f2c62835d8dc9b4ff450ea5662c409bdd64b
“[firstname <tomaspurvinys@google.com>
2024-01-01 22:11:01 +0200

    1.0v

--- e2e/Overview.spec.ts
+++ e2e/Overview.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('should display "This month items registered:"', async ({ page }) => {
-  await page.goto('http://localhost:5173/'); 
-  const totalItemsText = await page.textContent('h5');
-  expect(totalItemsText).toContain('This month items registered:');
+test('should display "items registered:"', async ({ page }) => {
+  await page.goto('http://localhost:5173'); 
+  const ItemsText = await page.textContent('h5');
+  expect(ItemsText).toContain('items registered:');
 });
